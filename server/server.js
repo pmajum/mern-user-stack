db6ba5744c33fa8a1e04b4ea377f4212821aab38
Prasenjit Majumder <pramaj08@in.ibm.com>
2019-10-14 10:33:58 +0530

    Mongo DB add

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ app.listen(config.port, (err) => {
 
 console.info('Mongo DB connection %s', config.mongoUri)
 
-/*mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri)
 
 mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`)
-})*/
+  throw new Error('unable to connect to database')
+})
