4d02f18bc7610aaf6d815404ad732068fcfc8cdf
Prasenjit Majumder <pramaj08@in.ibm.com>
2019-10-14 11:41:43 +0530

    User Backend Created

--- server/express.js
+++ server/express.js
@@ -6,6 +6,8 @@ import cors from 'cors'
 import helmet from 'helmet'
 import path from 'path'
 import Template from './../template'
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
@@ -19,8 +21,16 @@ app.use(helmet())
 app.use(cors())
 
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+app.use('/', authRoutes)
+app.use('/', userRoutes)
 app.get('/', (req, res) => {
   res.status(200).send(Template())
+})
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }
 })
 
 export default app
